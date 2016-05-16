180d3003f0eebd1b871f3042abb735c28983a2b2
ilmesi <nachoguason@gmail.com>
2016-05-16 12:27:42 +0200

    cleaning tests

--- test/interface-test.js
+++ test/interface-test.js
@@ -31,97 +31,73 @@ describe('Expected events', function () {
   it('should fail when a POST request occurs', function () {
     // Prepare
     postEvent.method = "POST";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
   it('should fail when a PUT request occurs', function () {
     // Prepare
     postEvent.method = "PUT";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
   it('should fail when a HEAD request occurs', function () {
     // Prepare
     postEvent.method = "HEAD";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
   it('should fail when a PATCH request occurs', function () {
     // Prepare
     postEvent.method = "PATCH";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
   it('should fail when a DELETE request occurs', function () {
     // Prepare
     postEvent.method = "DELETE";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
   it('should fail when a OPTIONS request occurs', function () {
     // Prepare
     postEvent.method = "OPTIONS";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
   
-    it('should fail when "event.method" is not a HTTP verb', function () {
+  it('should fail when "event.method" is not a HTTP verb', function () {
     // Prepare
     postEvent.method = "NOTAVERB";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
   
-    it('should fail when "event.size" is undefined', function () {
+  it('should fail when "event.size" is undefined', function () {
     // Prepare
     postEvent.method = "GET";
-    // Call
+    // Call & Assert
     expect(function () {
       app.handler(postEvent, postContext); 
-    }).to.throw();  
-    // Assert
-    sinon.assert.calledOnce(fail_spy);
-    sinon.assert.notCalled(succeed_spy);
+    }).to.throw();
   });
 
 });
